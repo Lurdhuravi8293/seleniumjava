47e12585aaf07a5d7333683986da6a3c5cabaf07
Lurdhuravi8293 <132303362+Lurdhuravi8293@users.noreply.github.com>
2025-01-24 08:43:22 -0700

    New code add by P

--- src/testdemo/Demo.java
+++ src/testdemo/Demo.java
@@ -8,6 +8,7 @@ public class Demo {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.get("https://www.techlearn.in");
 
 	}
 
