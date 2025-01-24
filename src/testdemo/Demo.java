ea0b003f07bfc515ac760b068373e8515bf5a249
Lurdhuravi8293 <132303362+Lurdhuravi8293@users.noreply.github.com>
2025-01-24 08:33:25 -0700

    Newchanges

--- src/testdemo/Demo.java
+++ src/testdemo/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 
 	}
 
---
+++ src/testdemo/Testdemo.java
@@ -0,0 +1,10 @@
+package testdemo;
+
+public class Testdemo {
+
+	public static void main(String[] args) {
+		System.out.println("gitcode");
+
+	}
+
+}
